021670af60bd78aa5de8daff5a09d966d1981c50
Fatih Ugur <fatihugur@gmail.com>
2021-04-15 16:38:15 +0200

    callback hell

--- ders-37/ders-37.js
+++ ders-37/ders-37.js
@@ -1,34 +1,33 @@
 // Callback Hell
 
-$.get('/ders-37-1.json', function (response) {
-  $.get('/ders-37-2.json', function (response2) {
+$.get("/ders-37-1.json", function (response) {
+  $.get("/ders-37-2.json", function (response2) {
     console.log(response.result + response2.result);
   });
 });
 
 // Promise
-var result1 = $.get('/ders-37-1.json')
-var result2 = $.get('/ders-37-2.json')
+var result1 = $.get("/ders-37-1.json");
+var result2 = $.get("/ders-37-2.json");
 
 Promise.all([result1, result2]).then(function ([x, y]) {
   console.log(x.result + y.result);
 });
 
-// function asyncFunction() {
-//   return new Promise((resolve, reject) => {
-//     resolve("fatih");
-//   })
-// }
+function asyncFunction() {
+  return new Promise((resolve, reject) => {
+    resolve("fatih");
+  });
+}
 
-// (async function () {
-//   console.log(await asyncFunction());
-// })();
+(async function () {
+  console.log(await asyncFunction());
+})();
 
 // Async Await
 (async () => {
-  var aresult1 = await $.get('/ders-37-1.json')
-  var aresult2 = await $.get('/ders-37-2.json')
+  var aresult1 = await $.get("/ders-37-1.json");
+  var aresult2 = await $.get("/ders-37-2.json");
 
-  console.log(aresult1.result + aresult2.result)
-})()
-
+  console.log(aresult1.result + aresult2.result);
+})();
